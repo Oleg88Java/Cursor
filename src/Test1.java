3d6f4e9c035be04dad4ab02f12fbf2670baf60a7
OlegKandalov <74624453+OlegKandalov@users.noreply.github.com>
2020-11-23 00:16:27 +0200

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,72 +1,100 @@
 //public class Test1 {
       //  public static void main(String[] args) {
             byte a = 3;
-            byte a1 = 4;
+        byte a1 = 4;
+        int sumA = a + a1;
+        int subA = a - a1;
+        int mulA = a * a1;
+        int divA = a / a1;
 
-            short b = 45;
-            short b1 = 6;
+        short b = 45;
+        short b1 = 6;
+        int sumB = b + b1;
+        int subB = b - b1;
+        int mulB = b * b1;
+        int divB = b / b1;
 
-            int c = 398;
-            int c1 = 46;
+        int c = 398;
+        int c1 = 46;
+        int sumC = c + c1;
+        int subC = c - c1;
+        int mulC = c * c1;
+        int divC = c / c1;
 
-            float d = 14.6F;
-            float d1 = 3.5F;
+        float d = 14.6F;
+        float d1 = 3.5F;
+        double sumD = d + d1;
+        double subD = d - d1;
+        double mulD = d * d1;
+        double divD = d / d1;
 
-            double e = 2.4;
-            double e1 = 2.9;
+        double e = 2.4;
+        double e1 = 2.9;
+        double sumE = e + e1;
+        double subE = e - e1;
+        double mulE = e * e1;
+        double divE = e / e1;
 
-            long f = 34565433L;
-            long f1 = 43L;
+        long f = 34565433L;
+        long f1 = 43L;
+        long sumF = f + f1;
+        long subF = f - f1;
+        long mulF = f * f1;
+        long divF = f / f1;
 
-            char g = '2';
-            char g1 = 3;
+        char g = '2';
+        char g1 = 3;
+        char sumG = (char) (g + g1);
+        char subG = (char) (g - g1);
+        char mulG = (char) (g * g1);
+        char divG = (char) (g / g1);
 
-            System.out.println("byte:");
-            System.out.println("Сумма " + (a + a1));
-            System.out.println("Вычетание  " + (a - a1));
-            System.out.println("Умножение " + (a * a1));
-            System.out.println("Деление " + (a / a1));
-            System.out.println();
+        System.out.println("byte:");
+        System.out.println("addition " + sumA);
+        System.out.println("subtraction  " + subA);
+        System.out.println("multyplycation " + mulA);
+        System.out.println("divition " + divA);
+        System.out.println();
 
-            System.out.println("short:");
-            System.out.println("Сумма " + (b + b1));
-            System.out.println("Вычетание  " + (b - b1));
-            System.out.println("Умножение " + (b * b1));
-            System.out.println("Деление " + (b / b1));
-            System.out.println();
+        System.out.println("short:");
+        System.out.println("addition " + sumB);
+        System.out.println("subtraction  " + subB);
+        System.out.println("multyplycation " + mulB);
+        System.out.println("divition " + divB);
+        System.out.println();
 
-            System.out.println("int:");
-            System.out.println("Сумма " + (c + c1));
-            System.out.println("Вычетание  " + (c - c1));
-            System.out.println("Умножение " + (c * c1));
-            System.out.println("Деление " + (c / c1));
-            System.out.println();
+        System.out.println("int:");
+        System.out.println("addition " + sumC);
+        System.out.println("subtraction  " + subC);
+        System.out.println("multyplycation " + mulC);
+        System.out.println("divition " + divC);
+        System.out.println();
 
-            System.out.println("float:");
-            System.out.println("Сумма " + (d + d1));
-            System.out.println("Вычетание  " + (d - d1));
-            System.out.println("Умножение " + (d * d1));
-            System.out.println("Деление " + (d / d1));
-            System.out.println();
+        System.out.println("float:");
+        System.out.println("addition " + sumD);
+        System.out.println("subtraction  " + subD);
+        System.out.println("multyplycation " + mulD);
+        System.out.println("divition " + divD);
+        System.out.println();
 
-            System.out.println("double:");
-            System.out.println("Сумма " + (e + e1));
-            System.out.println("Вычетание  " + (e - e1));
-            System.out.println("Умножение " + (e * e1));
-            System.out.println("Деление " + (e / e1));
-            System.out.println();
+        System.out.println("double:");
+        System.out.println("addition " + sumE);
+        System.out.println("subtraction  " + subE);
+        System.out.println("multyplycation " + mulE);
+        System.out.println("divition " + divE);
+        System.out.println();
 
-            System.out.println("long:");
-            System.out.println("Сумма " + (f + f1));
-            System.out.println("Вычетание  " + (f - f1));
-            System.out.println("Умножение " + (f * f1));
-            System.out.println("Деление " + (f / f1));
-            System.out.println();
+        System.out.println("long:");
+        System.out.println("addition " + sumF);
+        System.out.println("subtraction  " + subF);
+        System.out.println("multyplycation " + mulF);
+        System.out.println("divition " + divF);
+        System.out.println();
 
-            System.out.println("char:");
-            System.out.println("Сумма " + (g + g1));
-            System.out.println("Вычетание  " + (g - g1));
-            System.out.println("Умножение " + (g * g1));
-            System.out.println("Деление " + (g / g1));
-        }
+        System.out.println("char:");
+        System.out.println("addition " + sumG);
+        System.out.println("subtraction  " + subG);
+        System.out.println("multyplycation " + mulG);
+        System.out.println("divition " + divG);
+    }
 }
